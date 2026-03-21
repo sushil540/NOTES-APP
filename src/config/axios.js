59c21c3fc5cfa52de522697d6c136eb5d947ed9a
Sushilendra G P <77796514+sushil540@users.noreply.github.com>
2026-03-21 14:35:44 +0530

    Change axios base URL to production environment

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export default axios.create({baseURL:'http://127.0.0.1:3000'})
+export default axios.create({baseURL:"https://notes-app-backend-production-336b.up.railway.app/"})//'http://127.0.0.1:3000'
